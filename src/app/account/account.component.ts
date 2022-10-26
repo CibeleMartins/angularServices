72b0c709ff85e337a1a3dfd05bb9f635ad177afc
Cibele Martins <Cibele.Martins@ibm.com>
2022-10-26 10:54:01 -0300

    🚧 feat: understanding the service hierarchy

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input} from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-account',
@@ -10,16 +11,13 @@ import { LoggingService } from '../logging.service';
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private logging: LoggingService) {
+  constructor(private logging: LoggingService, private accountService: AccountService) {
 
   }
 
-
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
-    
+    this.accountService.onStatusChanged(this.id, status)
     this.logging.changeStatus(status)
   }
   
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -6,7 +6,7 @@ import { AccountService } from '../account.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountService]
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
 
